88f550c5437b01fc3be4cd737ea1afbabf9bdf4e
emokido <gusah0p@gmail.com>
2023-04-11 22:14:32 +0900

    ValueError fix

--- calc.py
+++ calc.py
@@ -12,36 +12,39 @@ def div(x, y):
 # main function
 def main():
     check = 1
-    print("Welcome to calcuator")
-    while check >= 1:        
-        print("0: exit, 1: plus 2:subtract 3:multiplication 4:divide")
-        check = int(input())
-        if check == 1:
-            print("First Number")
-            x = float(input())
-            print("Second Number")
-            y = float(input())
-            print("answer : ", plus(x,y))
-        elif check == 2:
-            print("First Number")
-            x = float(input())
-            print("Second Number")
-            y = float(input())
-            print("answer : ", sub(x,y))
-        elif check == 3:
-            print("First Number")
-            x = float(input())
-            print("Second Number")
-            y = float(input())
-            print("answer : ", mul(x,y))
-        elif check == 4:
-            print("First Number")
-            x = float(input())
-            print("Second Number")
-            y = float(input())
-            print("answer : ", div(x,y))
-        elif check>4:
-            print("Unsupported")
+    print("Welcome to calculator")
+    while check >= 1:
+        print("0: exit, 1: plus 2: subtract 3: multiplication 4: divide")
+        try:
+            check = int(input())
+            if check == 1:
+                print("First Number:")
+                x = float(input())
+                print("Second Number:")
+                y = float(input())
+                print("Answer:", plus(x, y))
+            elif check == 2:
+                print("First Number:")
+                x = float(input())
+                print("Second Number:")
+                y = float(input())
+                print("Answer:", sub(x, y))
+            elif check == 3:
+                print("First Number:")
+                x = float(input())
+                print("Second Number:")
+                y = float(input())
+                print("Answer:", mul(x, y))
+            elif check == 4:
+                print("First Number:")
+                x = float(input())
+                print("Second Number:")
+                y = float(input())
+                print("Answer:", div(x, y))
+            elif check > 4:
+                print("Unsupported")
+        except ValueError:
+            print("숫자를 입력하세요")
 
 if __name__ == "__main__":
     main()
